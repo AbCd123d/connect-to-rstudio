bc7c0fbb28185d4c7694a8f7434caa2e1ce43739
AbCd123d <njms168@gmail.com>
2021-06-15 09:07:50 -0700

    Add comments on steps

--- testing.R
+++ testing.R
@@ -1,4 +1,9 @@
+#Link: https://www.coursera.org/learn/data-scientists-tools/lecture/MBW5h/linking-github-and-r-studio
+#Step 1: Connect to GitHub:  File - New Project - Version Control 
+#Step 2: Get into the desired repositories 
+#Step 3: Pull file from the repositories 
+          #OR Push file to the repositories: Git Tab - Commit - stage - Commit message - push
+
 print('This project is created in RStudio')
 print('And now it lives on GitHub')
-
 print('update on 06/15 using another PC')
